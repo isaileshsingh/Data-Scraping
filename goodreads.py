2733a782b62b52c86a99de43023abf3901bbe91d
Sailesh Singh <104683889+isaileshsingh@users.noreply.github.com>
2025-01-02 08:17:39 +0545

    added a layer of scraping within div-s

--- goodreads.py
+++ goodreads.py
@@ -10,14 +10,27 @@ def scrape_quotes(url):
 
     results = []
     for quote in quotes:
+
         quote_text = quote.find("div", _class= "quoteText").text
         details = {}
         details["quote"] = quote_text.split("-")[0].strip()
         details["author"] = quote_text.split("-")[1].strip()
+
+        # tags = quote.find_all("div", _class="smallText")
+        tags = quote.find("div", _class="smallText")
+        try:
+            details["tags"] = tags.text.replace("tags:","")
+        except Exception as e:
+            # print(e)
+            pass
+
+
         results.append(details)
 
     return results
 
 if __name__ == "__main__":
     result = scrape_quotes("https://www.goodreads.com/quotes")
+    quote = result[10]
+    tags = quote["tags"]
     print(result)
